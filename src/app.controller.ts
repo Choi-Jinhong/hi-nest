4e5b0747dcbe60f964d98c143b1e2d315baf9417
Gnong <jinhongdev@gmail.com>
2021-03-02 21:17:12 +0900

    Explain Decoration

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post("/hello") // 데코레이션, 아래 함수와 떨어지면 안됨
+  sayHello(): string {
+    return 'Hello Everyone';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController], // 라우터와 같은 존재, URL을 가지고오는 ㅇ겨할
   providers: [AppService],
 })
 export class AppModule {}
