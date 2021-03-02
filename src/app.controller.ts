bccf95db3429ad673ace795c2880a7004b177b07
Gnong <jinhongdev@gmail.com>
2021-03-02 21:18:37 +0900

    Explain Decoration(2)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -10,7 +10,7 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Post("/hello") // 데코레이션, 아래 함수와 떨어지면 안됨
+  @Get("/hello") // 데코레이션, 함수와 떨어질 경우 오류를 발생
   sayHello(): string {
     return 'Hello Everyone';
   }
