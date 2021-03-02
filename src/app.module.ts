094c2115fd5df4c929aa0804a198d50ae4df58d1
Gnong <jinhongdev@gmail.com>
2021-03-02 21:46:31 +0900

    Learn Decorate about Controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MoviesController } from './movies/movies.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController], // 라우터와 같은 존재, URL을 가지고오는 ㅇ겨할
-  providers: [AppService],
+  controllers: [MoviesController], // 라우터와 같은 존재, URL을 가지고오는 ㅇ겨할
+  providers: [],
 })
 export class AppModule {}
